82dbe4f199217afc6b276160ca9a54b07479ef82
BrianFernandoJara <brianfernandojara@gmail.com>
2023-10-04 09:00:54 -0400

    Loops complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i >= 0:
+        if(i == 0):
+            print("Happy New Year!")
+            return i
+        print(f"{i}")
+        i = i - 1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_list = [number * number for number in int_list]
+    return new_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for e in range(100):
+        e = e + 1
+        if(e % 3 == 0 and e % 5 == 0):
+            print("FizzBuzz")
+        elif(e % 3 == 0):
+            print("Fizz")
+        elif(e % 5 == 0):
+            print("Buzz")
+        else:
+            print(e)
